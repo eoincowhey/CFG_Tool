f71ae997c2a30ed12bea4568206b0b340a509d9f
eoincowhey <eoincowhey@hotmail.com>
2022-11-08 00:36:00 +0000

    V20

--- main.py
+++ main.py
@@ -47,9 +47,9 @@ Z_rate = ((Z_Start-Z_Finish)/T_swing)/2
 I_minimum = Source_I1-Source_I2
 
 if CT_Star_Point == "Busbar":
-    Busbar_comp = math.radians(180)
+    Busbar_comp = 180
 else:
-    Busbar_comp = math.radians(0)
+    Busbar_comp = 0
 
 
 # Fault record process data
@@ -60,7 +60,7 @@ No_of_samples = T_swing * Sample_rate
 Sample_t_res = 1/Sample_rate
 
 
-# Functions
+# Functions 1
 
 # Function 1: used to generate lists for sample number and sample time stamp
 # k = offset, samples = max value limit, delta = delta sample
@@ -79,32 +79,40 @@ Time_Stamp = list_range(Sample_t_res, (No_of_samples * Sample_t_res), Sample_t_r
 Calcs = pd.DataFrame(Time_Stamp, columns = ['Time Stamp'])
 
 
-# Power Swing Impedance Calculations
+# Functions 2
 
+def Inst(Source, freq, df, phi, phase, starpoint):
+    return(Source * np.sin((2 * math.pi * freq * df) + math.radians(phi) + math.radians(phase)
+                           + math.radians(starpoint)))
+
+
+
+
+# Power Swing Impedance Calculations
 # R Phase
-Calcs['V1_R'] = Source_V1 * np.sin((2 * math.pi * Source_1_Freq * Calcs['Time Stamp']) + math.radians(Source_1_Voltage_Phi))
-Calcs['V2_R'] = Source_V2 * np.sin((2 * math.pi * Source_2_Freq * Calcs['Time Stamp']) + math.radians(Source_2_Voltage_Phi))
+Calcs['V1_R'] = Inst(Source_V1, Source_1_Freq, Calcs['Time Stamp'], Source_1_Voltage_Phi, 0, 0)
+Calcs['V2_R'] = Inst(Source_V2, Source_2_Freq, Calcs['Time Stamp'], Source_2_Voltage_Phi, 0, 0)
 Calcs['V1_R + V2_R'] = Calcs['V1_R'] + Calcs['V2_R']
-Calcs['I1_R'] = Source_I1 * np.sin((2 * math.pi * Source_1_Freq * Calcs['Time Stamp']) + math.radians(Source_1_Current_Phi) + Busbar_comp)
-Calcs['I2_R'] = Source_I2 * np.sin((2 * math.pi * Source_2_Freq * Calcs['Time Stamp']) + math.radians(Source_2_Current_Phi) + Busbar_comp)
+Calcs['I1_R'] = Inst(Source_I1, Source_1_Freq, Calcs['Time Stamp'], Source_1_Current_Phi, 0, Busbar_comp)
+Calcs['I2_R'] = Inst(Source_I2, Source_2_Freq, Calcs['Time Stamp'], Source_2_Current_Phi, 0, Busbar_comp)
 Calcs['I1_R + I2_R'] = Calcs['I1_R'] + Calcs['I2_R']
 Calcs['Z_R'] = Calcs['V1_R + V2_R'] / Calcs['I1_R + I2_R']
 
 # S Phase
-Calcs['V1_S'] = Source_V1 * np.sin((2 * math.pi * Source_1_Freq * Calcs['Time Stamp']) + math.radians(Source_1_Voltage_Phi) + math.radians(240))
-Calcs['V2_S'] = Source_V2 * np.sin((2 * math.pi * Source_2_Freq * Calcs['Time Stamp']) + math.radians(Source_2_Voltage_Phi) + math.radians(240))
+Calcs['V1_S'] = Inst(Source_V1, Source_1_Freq, Calcs['Time Stamp'], Source_1_Voltage_Phi, 240, 0)
+Calcs['V2_S'] = Inst(Source_V2, Source_2_Freq, Calcs['Time Stamp'], Source_2_Voltage_Phi, 240, 0)
 Calcs['V1_S + V2_S'] = Calcs['V1_S'] + Calcs['V2_S']
-Calcs['I1_S'] = Source_I1 * np.sin((2 * math.pi * Source_1_Freq * Calcs['Time Stamp']) + math.radians(Source_1_Current_Phi) + math.radians(240) + Busbar_comp)
-Calcs['I2_S'] = Source_I2 * np.sin((2 * math.pi * Source_2_Freq * Calcs['Time Stamp']) + math.radians(Source_2_Current_Phi) + math.radians(240) + Busbar_comp)
+Calcs['I1_S'] = Inst(Source_I1, Source_1_Freq, Calcs['Time Stamp'], Source_1_Current_Phi, 240, Busbar_comp)
+Calcs['I2_S'] = Inst(Source_I2, Source_2_Freq, Calcs['Time Stamp'], Source_2_Current_Phi, 240, Busbar_comp)
 Calcs['I1_S + I2_S'] = Calcs['I1_S'] + Calcs['I2_S']
 Calcs['Z_S'] = Calcs['V1_S + V2_S'] / Calcs['I1_S + I2_S']
 
 # T Phase
-Calcs['V1_T'] = Source_V1 * np.sin((2 * math.pi * Source_1_Freq * Calcs['Time Stamp']) + math.radians(Source_1_Voltage_Phi) + math.radians(120))
-Calcs['V2_T'] = Source_V2 * np.sin((2 * math.pi * Source_2_Freq * Calcs['Time Stamp']) + math.radians(Source_2_Voltage_Phi) + math.radians(120))
+Calcs['V1_T'] = Inst(Source_V1, Source_1_Freq, Calcs['Time Stamp'], Source_1_Voltage_Phi, 120, 0)
+Calcs['V2_T'] = Inst(Source_V2, Source_2_Freq, Calcs['Time Stamp'], Source_2_Voltage_Phi, 120, 0)
 Calcs['V1_T + V2_T'] = Calcs['V1_T'] + Calcs['V2_T']
-Calcs['I1_T'] = Source_I1 * np.sin((2 * math.pi * Source_1_Freq * Calcs['Time Stamp']) + math.radians(Source_1_Current_Phi) + math.radians(120) + Busbar_comp)
-Calcs['I2_T'] = Source_I2 * np.sin((2 * math.pi * Source_2_Freq * Calcs['Time Stamp']) + math.radians(Source_2_Current_Phi) + math.radians(120) + Busbar_comp)
+Calcs['I1_T'] = Inst(Source_I1, Source_1_Freq, Calcs['Time Stamp'], Source_1_Current_Phi, 120, Busbar_comp)
+Calcs['I2_T'] = Inst(Source_I2, Source_2_Freq, Calcs['Time Stamp'], Source_2_Current_Phi, 120, Busbar_comp)
 Calcs['I1_T + I2_T'] = Calcs['I1_T'] + Calcs['I2_T']
 Calcs['Z_T'] = Calcs['V1_T + V2_T'] / Calcs['I1_T + I2_T']
 
@@ -145,32 +153,39 @@ IT_Multiplier = IT_Range/Recording_decimal
 
 # Dat File
 
-Calcs['VR_dat'] = (((Calcs['V1_R + V2_R'] - VR_Min)/VR_Range) * Recording_decimal).astype('int')
-Calcs['VS_dat'] = (((Calcs['V1_S + V2_S'] - VS_Min)/VS_Range) * Recording_decimal).astype('int')
-Calcs['VT_dat'] = (((Calcs['V1_T + V2_T'] - VT_Min)/VT_Range) * Recording_decimal).astype('int')
-Calcs['IR_dat'] = (((Calcs['I1_R + I2_R'] - IR_Min)/IR_Range) * Recording_decimal).astype('int')
-Calcs['IS_dat'] = (((Calcs['I1_S + I2_S'] - IS_Min)/IS_Range) * Recording_decimal).astype('int')
-Calcs['IT_dat'] = (((Calcs['I1_T + I2_T'] - IT_Min)/IT_Range) * Recording_decimal).astype('int')
+def int_conv(df, Min, Range, Rec_dec):
+    return((((df - Min)/Range) * Rec_dec).astype('int'))
 
+
+Calcs['VR_dat'] = int_conv(Calcs['V1_R + V2_R'], VR_Min, VR_Range, Recording_decimal)
+Calcs['VS_dat'] = int_conv(Calcs['V1_S + V2_S'], VS_Min, VS_Range, Recording_decimal)
+Calcs['VT_dat'] = int_conv(Calcs['V1_T + V2_T'], VT_Min, VT_Range, Recording_decimal)
+Calcs['IR_dat'] = int_conv(Calcs['I1_R + I2_R'], VR_Min, VR_Range, Recording_decimal)
+Calcs['IS_dat'] = int_conv(Calcs['I1_S + I2_S'], VS_Min, VS_Range, Recording_decimal)
+Calcs['IT_dat'] = int_conv(Calcs['I1_T + I2_T'], VT_Min, VT_Range, Recording_decimal)
 
 
 Dat_File = pd.DataFrame(Time_Stamp, columns = ['Time Stamp'])
 Dat_File = Dat_File.merge(Calcs, on='Time Stamp', how='left')
 
-Dat_File = Dat_File.drop(['V1_R', 'V2_R', 'V1_R + V2_R','I1_R', 'I2_R', 'I1_R + I2_R', 'Z_R'], axis = 'columns')
-Dat_File = Dat_File.drop(['V1_S', 'V2_S', 'V1_S + V2_S','I1_S', 'I2_S', 'I1_S + I2_S', 'Z_S'], axis = 'columns')
-Dat_File = Dat_File.drop(['V1_T', 'V2_T', 'V1_T + V2_T','I1_T', 'I2_T', 'I1_T + I2_T', 'Z_T'], axis = 'columns')
+Dat_File = Dat_File.drop(['V1_R', 'V2_R', 'V1_R + V2_R', 'I1_R', 'I2_R', 'I1_R + I2_R', 'Z_R'], axis = 'columns')
+Dat_File = Dat_File.drop(['V1_S', 'V2_S', 'V1_S + V2_S', 'I1_S', 'I2_S', 'I1_S + I2_S', 'Z_S'], axis = 'columns')
+Dat_File = Dat_File.drop(['V1_T', 'V2_T', 'V1_T + V2_T', 'I1_T', 'I2_T', 'I1_T + I2_T', 'Z_T'], axis = 'columns')
 Dat_File['Time Stamp'] = (Dat_File['Time Stamp']*1000000).astype(int)
 
 
 # CFG File
 
-Output_1 = ['1',',','VL1',',','1',',',',','V',',',VR_Multiplier,',',VR_Min,',','0.00E+00',',','0',',',Recording_decimal,',',VTP,',',VTS,',','s']
-Output_2 = ['2',',','VL2',',','2',',',',','V',',',VS_Multiplier,',',VS_Min,',','0.00E+00',',','0',',',Recording_decimal,',',VTP,',',VTS,',','s']
-Output_3 = ['3',',','VL3',',','3',',',',','V',',',VT_Multiplier,',',VT_Min,',','0.00E+00',',','0',',',Recording_decimal,',',VTP,',',VTS,',','s']
-Output_4 = ['4',',','IL1',',','4',',',',','A',',',IR_Multiplier,',',IR_Min,',','0.00E+00',',','0',',',Recording_decimal,',',CTP,',',CTS,',','s']
-Output_5 = ['5',',','IL2',',','5',',',',','A',',',IS_Multiplier,',',IS_Min,',','0.00E+00',',','0',',',Recording_decimal,',',CTP,',',CTS,',','s']
-Output_6 = ['6',',','IL3',',','6',',',',','A',',',IT_Multiplier,',',IT_Min,',','0.00E+00',',','0',',',Recording_decimal,',',CTP,',',CTS,',','s']
+def frame_op(No1, Name, No2, var, Mult, Min, Rec_dec, ITP, ITS):
+    return([No1,',',Name,',',No2,',',',',var,',',Mult,',',Min,',','0.00E+00',',','0',',',Rec_dec,',',ITP,',',ITS,',','s'])
+
+
+Output_1 = frame_op(1, 'VL1', 1, 'V', VR_Multiplier, VR_Min, Recording_decimal, VTP, VTS)
+Output_2 = frame_op(2, 'VL2', 1, 'V', VS_Multiplier, VS_Min, Recording_decimal, VTP, VTS)
+Output_3 = frame_op(3, 'VL3', 1, 'V', VT_Multiplier, VT_Min, Recording_decimal, VTP, VTS)
+Output_4 = frame_op(4, 'IL1', 1, 'A', IR_Multiplier, IR_Min, Recording_decimal, CTP, CTS)
+Output_5 = frame_op(5, 'IL2', 1, 'A', IS_Multiplier, IS_Min, Recording_decimal, CTP, CTS)
+Output_6 = frame_op(6, 'IL3', 1, 'A', IT_Multiplier, IT_Min, Recording_decimal, CTP, CTS)
 
 Outputs = pd.DataFrame([Output_1, Output_2, Output_3, Output_4, Output_5, Output_6])
 
